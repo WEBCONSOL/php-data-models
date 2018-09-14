c253a78c564961ddd1446de2d60240390f47533c
Sothea Nim <nimsothea@gmail.com>
2018-09-14 15:02:01 -0400

    initial commit

---
+++ src/WC/Models/ListModel.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace WC\Models;
+
+class ListModel implements \JsonSerializable, \Countable
+{
+    private $valueType = null;
+    private $data = array();
+
+    function __construct($val)
+    {
+        $this->reset($val);
+    }
+
+    public function reset($v=null) {
+        if (is_array($v)) {
+            $this->data = $v;
+            $this->valueType = 'array';
+        }
+        else if (is_object($v)) {
+            $this->data = json_decode(json_encode($v), true);
+            $this->valueType = 'object';
+        }
+        else if (file_exists($v) && pathinfo($v, PATHINFO_EXTENSION)) {
+            $this->data = json_decode(file_get_contents($v), true);
+            $this->valueType = 'file';
+        }
+        else {
+            $this->data = array($this->data);
+            $this->valueType = 'string';
+        }
+    }
+
+    public function merge(array $list) {
+        foreach ($list as $key=>$value) {
+            $this->data[$key] = $value;
+        }
+    }
+
+    public function get($k=null, $defult=null) {
+        if ($k !== null && isset($this->data[$k])) {
+            return $this->data[$k];
+        }
+        else if ($k===null && $this->valueType === 'string') {
+            return end($this->data);
+        }
+        return $defult;
+    }
+
+    public function set(string $k, $v) {
+        if ($k !== null) {
+            $this->data[$k] = $v;
+        }
+    }
+
+    public function remove() {
+        $argc = func_get_args();
+        if ($argc != null && sizeof($argc)) {
+            foreach ($argc as $k) {
+                if (isset($this->data[$k])) {
+                    unset($this->data[$k]);
+                }
+            }
+        }
+    }
+
+    public function has($k): bool {
+        return $k !== null && isset($this->data[$k]);
+    }
+
+    public function is($k, $v): bool {
+        return $k !== null && isset($this->data[$k]) && $this->data[$k] === $v;
+    }
+
+    public function first() {
+        if (sizeof($this->data)) {
+            return array_values($this->data)[0];
+        }
+        return null;
+    }
+
+    public function last() {
+        if (sizeof($this->data)) {
+            $arr = array_values($this->data);
+            return end($arr);
+        }
+        return null;
+    }
+
+    public function jsonSerialize()
+    {
+        return $this->data;
+    }
+
+    public function count(): int
+    {
+        return sizeof($this->data);
+    }
+
+    public function hasElement(): bool {
+        return $this->count() > 0;
+    }
+
+    public function isEmpty(): bool {
+        return !$this->hasElement();
+    }
+
+    public function getAsArray(): array {
+        return $this->data;
+    }
+}
