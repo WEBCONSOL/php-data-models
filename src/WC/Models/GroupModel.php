95501925ba2cd88d57086b036fff6530ceea182e
Sothea Nim <nimsothea@gmail.com>
2018-09-16 20:44:53 -0400

    fixed datetime typos

--- src/WC/Models/GroupModel.php
+++ src/WC/Models/GroupModel.php
@@ -4,12 +4,14 @@ namespace WC\Models;
 
 final class GroupModel extends BaseModel
 {
-    protected $requiredFields = array('id', 'name', 'title', 'created_datatime', 'last_modified_datatime');
+    protected $requiredFields = array('id', 'name', 'title', 'created_by', 'last_modified_by', 'created_datetime', 'last_modified_datetime');
     public function getId() {return $this->get('id');}
     public function getName(): string {return $this->get('username', '');}
     public function getTitle(): string {return $this->get('email', '');}
-    public function getCreatedDatetime(): int {return (int)$this->get('created_datatime', '0');}
-    public function getLastModifiedDatetime(): int {return (int)$this->get('last_modified_datatime', '0');}
+    public function getCreatedBy(): int {return (int)$this->get('created_by', '0');}
+    public function getLastModifiedBy(): int {return (int)$this->get('last_modified_by', '0');}
+    public function getCreatedDatetime(): int {return (int)$this->get('created_datetime', '0');}
+    public function getLastModifiedDatetime(): int {return (int)$this->get('last_modified_datetime', '0');}
     public function getGroups(): GroupsModel {return $this->get('groups', new GroupsModel(array()));}
     public function getPermissions(): PermissionsModel {return $this->get('permissions', new PermissionsModel(array()));}
     public function isAMemberOf($idOrName): bool {
